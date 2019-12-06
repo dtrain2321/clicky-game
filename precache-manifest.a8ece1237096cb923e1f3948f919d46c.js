feadb6438d9ab9463b88474ec8f01564fed4da1b
Derek Stewart <derek.charles.stewart@gmail.com>
2019-12-05 21:15:06 -0500

    Updates

---
+++ precache-manifest.a8ece1237096cb923e1f3948f919d46c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a05f4e273a446ad97482d9f2483093",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b022a23e7c8ba2f9c9a3",
+    "url": "/clicky-game/static/css/main.d9cb933a.chunk.css"
+  },
+  {
+    "revision": "8a20fc7804ea6b1efb9c",
+    "url": "/clicky-game/static/js/2.f3e0396d.chunk.js"
+  },
+  {
+    "revision": "b022a23e7c8ba2f9c9a3",
+    "url": "/clicky-game/static/js/main.cfcf4fde.chunk.js"
+  },
+  {
+    "revision": "04e84f1dcdef86a8fe21",
+    "url": "/clicky-game/static/js/runtime-main.80916c69.js"
+  }
+]);
